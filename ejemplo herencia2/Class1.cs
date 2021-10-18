bf49939d4074ad19bbf5b1b9535d73154d9a405e
Efrain <Efrain@EFRAIN-VAIO>
2021-10-18 08:27:37 -0500

    Add project files.

---
+++ ejemplo herencia2/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace ejemplo_herencia2
+{
+    class Alumno : Persona
+
+    {
+        private double matricula;
+        private string Carrera;
+        public string Carrera
+         {
+        get
+             {
+            return Carrera ;
+            }
+            set
+            {
+                Carrera = value;
+            }
+        }
+            public double matricula
+        {
+            get
+            {
+                return matricula;
+        }
+              set
+            {
+                matricula = value;
+            }
+        }
+
+        public Alumno()
+            {
+            matricula = 0.00;
+            Carrera = "";
+        }
+
+
---
+++ ejemplo herencia2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using.Sysem.Threading.Tasks;
+namespace ejemplo_herencia2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = newPersona();
+            persona.FechadeNacimiento = 04 / 12 / 20;
+            persona.Nombre = "";
+            Alumno Alm = newAlumno();
+            Console.WriteLine(Alm.ToString());
+        }
+    }
+}
